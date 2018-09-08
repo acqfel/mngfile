69e0c35e42bb0aea4653ee1d5eec5eb7147b3b62
Felipe Acquati <unomais@yahoo.com.br>
2018-09-08 18:43:28 +0000

    initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FileEditComponent } from './file-edit/file-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    FileEditComponent
   ],
   imports: [
     BrowserModule
